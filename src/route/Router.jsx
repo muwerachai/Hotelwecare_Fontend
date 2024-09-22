1d1c93daa9ee547a754826216388c24906720451
werachai <>
2024-09-22 14:06:39 +0700

    feat: login regis modal

--- src/route/Router.jsx
+++ src/route/Router.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import ReserveComponent from "../components/ReserveComponent";
+import EmailLoginModal from "../components/Modal/EmailLoginModal";
+import LoginRegisterModal from "../components/Modal/LoginRegisterModal";
 import AuthLayout from "../layout/auth/AuthLayout";
 import HomeLayout from "../layout/auth/HomeLayout";
 import AccountPage from "../pages/AccountPage";
@@ -11,14 +12,16 @@ function Router() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomeLayout />}>
-          <Route path="/" element={<HomePage />} />
-        </Route>
         <Route path="/" element={<AuthLayout />}>
-          <Route path="/account" element={<AccountPage />} />
-          <Route path="/whitelist" element={<WhitelistPage />} />
-          <Route path="/confirmPayment" element={<ConfirmPaymentPage />} />
-          <Route path="/reserve" element={<ReserveComponent />} />
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/account" element={<AccountPage />}></Route>
+          <Route path="/whitelist" element={<WhitelistPage />}></Route>
+          <Route path="/login" element={<LoginRegisterModal />}></Route>
+          <Route path="/loginEmail" element={<EmailLoginModal />}></Route>
+          <Route
+            path="/confirmPayment"
+            element={<ConfirmPaymentPage />}
+          ></Route>
         </Route>
       </Routes>
     </>
