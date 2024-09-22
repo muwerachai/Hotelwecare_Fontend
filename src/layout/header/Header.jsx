e3ee8fab999a24d737c309863f494328657edbea
werachai <>
2024-09-23 06:17:25 +0700

    refactor home page

--- src/layout/header/Header.jsx
+++ src/layout/header/Header.jsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom";
 import Logo from "./Logo";
 import MenuHeader from "./MenuHeader";
 import Search from "./Search";
 function Header() {
   return (
-    <header className="z-10 px-10 bg-cyan-200 border-b sticky top-0 left-0 right-0 h-20 flex flex-row justify-between items-center flex-nowrap">
+    <header
+      className="z-10 px-10 bg-cyan-200  sticky top-0 left-0 right-0 h-20 flex flex-row justify-between items-center 
+      flex-nowrap"
+    >
       <div className="sm:basis-1/12 md:basis-1/12 lg:basis-2/12 xl:basis-2/12 xxl:basis-2/12">
-        <Logo />
+        <Link to={"/"}>
+          <Logo />
+        </Link>
       </div>
       <div className="sm:basis-3/12 md:basis-3/12 lg:basis-6/12 xl:basis-7/12 flex flex-row items-center justify-center">
         <Search />
