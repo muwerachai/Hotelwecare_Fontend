12bc58d5afa239c94a7f38e94e3317136e044846
werachai <>
2024-09-25 19:52:49 +0700

    add fn otp

--- src/layout/header/MenuDropDownUser.jsx
+++ src/layout/header/MenuDropDownUser.jsx
@@ -1,5 +1,7 @@
 import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
+import { Menu, MenuButton, MenuItem, Transition } from "@headlessui/react";
+import { Link } from "react-router-dom";
+
 function MenuDropDownAsUser() {
   function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -10,10 +12,10 @@ function MenuDropDownAsUser() {
       className="relative inline-block text-left hover:shadow hover:rounded-full"
     >
       <div className="border rounded-full flex flex-row w-auto py-2 px-4 shadow cursor-pointer">
-        <Menu.Button className="flex flex-row items-center justify-between rounded-full w-12 h-6">
+        <MenuButton className="flex flex-row items-center justify-between rounded-full w-12 h-6">
           <i className="fa-solid fa-bars text-gray-600 text-[1rem]"></i>
           <i className="fa-solid fa-circle-user text-gray-600 text-[1.6rem]"></i>
-        </Menu.Button>
+        </MenuButton>
       </div>
       <Transition
         as={Fragment}
@@ -24,66 +26,66 @@ function MenuDropDownAsUser() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <MenuItem className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#"
+            <MenuItem>
+              {({ isActive }) => (
+                <Link
+                  to="/whitelist/:id"
                   className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    isActive ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm"
                   )}
                 >
                   WishList
-                </a>
+                </Link>
               )}
-            </Menu.Item>
+            </MenuItem>
           </div>
           <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
+            <MenuItem>
+              {({ isActive }) => (
                 <a
                   href="#"
                   className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    isActive ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm"
                   )}
                 >
                   Account
                 </a>
               )}
-            </Menu.Item>
+            </MenuItem>
           </div>
           <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
+            <MenuItem>
+              {({ isActive }) => (
                 <a
                   href="#"
                   className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    isActive ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm"
                   )}
                 >
                   Help
                 </a>
               )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
+            </MenuItem>
+            <MenuItem>
+              {({ isActive }) => (
                 <a
                   href="#"
                   className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    isActive ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm"
                   )}
                 >
                   Log out
                 </a>
               )}
-            </Menu.Item>
+            </MenuItem>
           </div>
-        </Menu.Items>
+        </MenuItem>
       </Transition>
     </Menu>
   );
--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import ReserveCounterReducer from "./ReserveCounterSlice";
+import AuthSlice from "./AuthSlice";
 export const store = configureStore({
   reducer: {
-    reserveCounter: ReserveCounterReducer
+    auth: AuthSlice
   }
 });
